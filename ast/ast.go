4f299d36707a6383c1f33d8fcac29e4e584c4751
Eselasie <eselasieakpablie2005@gmail.com>
2026-08-03 15:58:59 -0700

    Add String methods for the Return and Expression Statement

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,8 @@
 package ast
 
 import (
+	"bytes"
+
 	"berries/token"
 )
 
@@ -70,4 +72,50 @@ type ExpressionStatement struct {
 func (es *ExpressionStatement) StatementNode() {}
 func (es *ExpressionStatement) TokenLiteral() string {
 	return es.Token.Literal
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral())
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString((";"))
+
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
