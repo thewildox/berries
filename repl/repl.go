b1ddb424285e7c65e379e56b67b6739d1880a6b8
Eselasie <eselasieakpablie2005@gmail.com>
2026-08-19 14:37:58 -0700

    Extend REPL to parse entered lines.

--- repl/repl.go
+++ repl/repl.go
@@ -2,30 +2,40 @@ package repl
 
 import (
 	"berries/lexer"
-	"berries/token"
+	"berries/parser"
 	"bufio"
 	"fmt"
 	"io"
 )
 
-const PROMT = ">>"
+const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, PROMT)
+		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
-
 		if !scanned {
 			return
 		}
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
