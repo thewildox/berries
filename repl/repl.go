f1eb24be7236614fcf1cd78029a9f3efdbd16eae
Eselasie <eselasieakpablie2005@gmail.com>
2026-08-24 13:15:58 -0700

    Evaluate parsed input in REPL and print the result.

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"berries/evaluator"
 	"berries/lexer"
 	"berries/parser"
 	"bufio"
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
