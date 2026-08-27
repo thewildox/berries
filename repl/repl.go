d51f89f3941cf053e87e6452e471e6f01da70461
Eselasie <eselasieakpablie2005@gmail.com>
2026-08-27 13:57:16 -0700

    Evaluate let statements and identifiers with environment bindings and unit tests.

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"berries/evaluator"
 	"berries/lexer"
+	"berries/object"
 	"berries/parser"
 	"bufio"
 	"fmt"
@@ -11,6 +12,7 @@ import (
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	PrintBanner(out)
 
@@ -32,7 +34,8 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		evaluated := evaluator.Eval(program)
+
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
