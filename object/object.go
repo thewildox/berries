1030fbc1318ba4032e275317560853b9755a1211
Eselasie <eselasieakpablie2005@gmail.com>
2026-09-13 21:28:10 -0700

    Add Builtin object type for built-in functions.

--- object/object.go
+++ object/object.go
@@ -15,6 +15,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type ObjectType string
@@ -45,6 +46,11 @@ type Function struct {
 
 type String struct {
 	Value string
+}
+type BuiltinFunction func(args ...Object) ObjectType
+
+type Builtin struct {
+	Fn BuiltinFunction
 }
 
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
@@ -87,3 +93,6 @@ func (f *Function) Inspect() string {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
